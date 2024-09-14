a56cd8723f5458eb23ba5c96c38239dba9d38120
Michihiro Kawada <m@llll.jp>
2024-09-14 22:25:42 +0900

    finished 02 3つの言語で翻訳してみよう

--- MyPortfolio/256/02 3つの言語で翻訳してみよう/js/main.js
+++ MyPortfolio/256/02 3つの言語で翻訳してみよう/js/main.js
@@ -3,22 +3,22 @@ const btn_en = document.getElementById("btn_en");
 const btn_sp = document.getElementById("btn_sp");
 const target = document.getElementById("target");
 
-btn_jp.addEventListener(click, () => {
+btn_jp.addEventListener("click", () => {
   target.textContent = "ありがとう！";
   btn_jp.disabled = true;
   btn_en.disabled = false;
   btn_sp.disabled = false;
 })
 
-btn_en.addEventListener(click, () => {
+btn_en.addEventListener("click", () => {
   target.textContent = "Thank you!";
   btn_jp.disabled = false;
   btn_en.disabled = true;
   btn_sp.disabled = false;
 })
 
-btn_sp.addEventListener(click, () => {
-  target.textContent = "Danke";
+btn_sp.addEventListener("click", () => {
+  target.textContent = "Gracias!";
   btn_jp.disabled = false;
   btn_en.disabled = false;
   btn_sp.disabled = true;
