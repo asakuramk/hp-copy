f28d26dc12b19dd65699ba5c532a8521aa9b7fd1
M Kawada <m@llll.jp>
2024-09-16 21:40:30 +0900

    text追加

--- 256/04コンテンツの折りたたみをしてみよう/js/main.js
+++ 256/04コンテンツの折りたたみをしてみよう/js/main.js
@@ -4,9 +4,11 @@ btn.addEventListener("click", () => {
 
   if (more.classList.contains("appear")) {
     more.classList.remove("appear")
+    btn.textContent="閉じる"
   }
   else {
     more.classList.add("appear");
+    btn.textContent="もっと読む"
   }
 }
 )
