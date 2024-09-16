626e5fac52a7ad6e1b94bcd975a16764e2fa9ec0
M Kawada <m@llll.jp>
2024-09-17 06:42:30 +0900

    ファイル調整

--- 256/05モードを切り替えてみよう/js/main.js
+++ 256/05モードを切り替えてみよう/js/main.js
@@ -5,16 +5,13 @@ light.classList.add("yellow-bg");
 light.addEventListener("click", () => {
   light.classList.add("yellow-bg");
   dark.classList.remove("yellow-bg");
-  
   document.querySelector("body").style.background = "white";
   document.querySelector("body").style.color = "black";
   })
 
-
 dark.addEventListener("click", () => {
   dark.classList.add("yellow-bg");
   light.classList.remove("yellow-bg");
-
   document.querySelector("body").style.background = "black";
   document.querySelector("body").style.color = "white";
 })
