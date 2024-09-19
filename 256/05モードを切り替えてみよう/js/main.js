abb3664997e6b648a74faa2c080cdcf65fff9a2e
Michihiro Kawada <m@llll.jp>
2024-09-20 07:30:01 +0900

    toggle文で修正

--- 256/05モードを切り替えてみよう/js/main.js
+++ 256/05モードを切り替えてみよう/js/main.js
@@ -1,17 +1,37 @@
 const light = document.getElementById("light");
 const dark = document.getElementById("dark");
+const body = document.querySelector("body");
 light.classList.add("yellow-bg");
 
-light.addEventListener("click", () => {
-  light.classList.add("yellow-bg");
-  dark.classList.remove("yellow-bg");
-  document.querySelector("body").style.background = "white";
-  document.querySelector("body").style.color = "black";
-  })
+// light.addEventListener("click", () => {
+//   light.classList.add("yellow-bg");
+//   dark.classList.remove("yellow-bg");
+//   document.querySelector("body").style.background = "white";
+//   document.querySelector("body").style.color = "black";
+//   })
 
-dark.addEventListener("click", () => {
-  dark.classList.add("yellow-bg");
-  light.classList.remove("yellow-bg");
-  document.querySelector("body").style.background = "black";
-  document.querySelector("body").style.color = "white";
+// dark.addEventListener("click", () => {
+//   dark.classList.add("yellow-bg");
+//   light.classList.remove("yellow-bg");
+//   document.querySelector("body").style.background = "black";
+//   document.querySelector("body").style.color = "white";
+// })
+
+const biElement = document.querySelectorAll(".bi");
+light.classList.toggle("color");
+
+function changeColor() {
+  light.classList.toggle("color");
+  dark.classList.toggle("color");
+  body.classList.toggle("color");
+}
+
+biElement.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (icon.id == "light") {
+      changeColor();
+    } else {
+      changeColor();
+    }
+  })
 })
