f1627887e5b1d53a250d1ef8971a3b8f0e8932c7
Michihiro Kawada <m@llll.jp>
2024-09-21 19:53:10 +0900

    分割代入

--- 256/動かして学ぶJavaScript Vol.2/01奇跡を起こしてみよう/js/main.js
+++ 256/動かして学ぶJavaScript Vol.2/01奇跡を起こしてみよう/js/main.js
@@ -3,8 +3,12 @@ const you = document.getElementById("you");
 const swap = document.getElementById("swap");
 let temp = "";
 
+// swap.addEventListener("click", () => {
+//   temp = me.textContent;
+//   me.textContent = you.textContent;
+//   you.textContent = temp;
+// })
+
 swap.addEventListener("click", () => {
-  temp = me.textContent;
-  me.textContent = you.textContent;
-  you.textContent = temp;
-})
+  [me.textContent, you.textContent] = [you.textContent, me.textContent]
+ })
