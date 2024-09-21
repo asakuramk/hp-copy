3f4dafbc43f87a38a62d9e87a5e6383d233b7cf3
Michihiro Kawada <m@llll.jp>
2024-09-21 21:01:09 +0900

    ?記法

--- 256/動かして学ぶJavaScript Vol.2/02文字数をカウントしてみよう/js/main.js
+++ 256/動かして学ぶJavaScript Vol.2/02文字数をカウントしてみよう/js/main.js
@@ -1,7 +1,20 @@
 const message = document.getElementById("message");
 var stat = document.getElementById("status");
 
-message.addEventListener("input", () => {
-  stat.textContent = `${message.value.length}文字入力されています。`;
+// message.addEventListener("input", () => {
+//   const num = message.value.length;
+//   if (num) {
+//     stat.textContent = `${message.value.length}文字入力されています。`;
+//   } else {
+//     stat.textContent = "";
+//   }
+// });
 
+
+message.addEventListener('input', () => {
+  const msgLength = message.value.length;
+  
+  stat.textContent = (msgLength == 0)
+    ? ''
+    :`${msgLength} 文字入力されています`;
 });
