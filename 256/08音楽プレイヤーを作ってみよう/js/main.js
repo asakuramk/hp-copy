78bfa3d3da035fad885c4b435be9b86537ff5270
Michihiro Kawada <m@llll.jp>
2024-09-21 11:14:30 +0900

    8 finished(if文)

--- 256/08音楽プレイヤーを作ってみよう/js/main.js
+++ 256/08音楽プレイヤーを作ってみよう/js/main.js
@@ -1,13 +1,26 @@
 const shuffle = document.getElementById("shuffle");
 const repeat = document.getElementById("repeat");
-
+let count = 0;
 
 
 shuffle.addEventListener("click", () => {
   shuffle.classList.toggle("color")
 });
 
+
 repeat.addEventListener("click", () => {
-  repeat.classList.add("bi-repeat-1");
-  repeat.classList.remove("bi-repeat")
+  if (count == 0) {
+    repeat.classList.add("color");
+    count ++;
+  } else if (count == 1) {
+    repeat.classList.add("bi-repeat-1");
+    repeat.classList.remove("bi-repeat");
+    count++;
+  } else {
+    repeat.classList.remove("color");
+    repeat.classList.remove("bi-repeat-1");
+    repeat.classList.add("bi-repeat");
+    count = 0;
+  }
+
 });
